8c3e9f7142cd14184914d568b9099524bebd18d1
Eric Geldmacher <egeldmacher@wustl.edu>
2021-03-10 13:48:09 -0600

    fix regression in create command when updating objects

--- nbcli/commands/tools.py
+++ nbcli/commands/tools.py
@@ -86,7 +86,7 @@ class NbArgs():
         for arg in nba.args:
             nba.update(res.lookup, arg)
 
-        result = ep.filter(**nba.kwargs, brief=1)
+        result = ep.filter(**nba.kwargs)
 
         self.apply_res(result, res)
 
