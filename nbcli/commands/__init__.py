435c2620c97504fffd4f4e341e226dfe313fef97
Eric Geldmacher <egeldmacher@wustl.edu>
2022-09-30 13:21:49 -0500

    change models subcommand to info subcommand

--- nbcli/commands/__init__.py
+++ nbcli/commands/__init__.py
@@ -3,7 +3,7 @@
 
 from nbcli.commands.base import BaseSubCommand      # noqa: F401
 from nbcli.commands.init import InitSubCommand      # noqa: F401
-from nbcli.commands.models import ModelsSubCommand  # noqa: F401
+from nbcli.commands.info import InfoSubCommand      # noqa: F401
 from nbcli.commands.search import SearchSubCommand  # noqa: F401
 from nbcli.commands.filter import FilterSubCommand  # noqa: F401
 from nbcli.commands.create import CreateSubCommand  # noqa: F401
