a8f863ae57f2b7a70102802391ba3acb28764da7
Eric Geldmacher <egeldmacher@wustl.edu>
2022-10-04 14:52:16 -0500

    update testing docs

--- nbcli/commands/info.py
+++ nbcli/commands/info.py
@@ -8,6 +8,8 @@ from nbcli import __version__ as nbcli_version
 from pynetbox import __version__ as pynb_version
 
 from os import environ
+from time import time, sleep
+from pprint import pprint
 
 
 class InfoSubCommand(BaseSubCommand):
@@ -19,14 +21,23 @@ class InfoSubCommand(BaseSubCommand):
     def setup(self):
         """Additional arguments for info command."""
 
-        self.parser.add_argument('--detailed',
-                                  action='store_true',
-                                  help='Show more detailed info.')
-        self.parser.add_argument('--models',
-                                 nargs='?',
-                                 default=None,
-                                 const='all',
-                                 help='Display detailed info on given model.')
+        info_opts = self.parser.add_mutually_exclusive_group()
+
+        info_opts.add_argument('--detailed',
+                               action='store_true',
+                               help='Show more detailed info.')
+        info_opts.add_argument('--models',
+                               nargs='?',
+                               default=None,
+                               const='all',
+                               help='Display detailed info on given model.')
+        info_opts.add_argument('--status',
+                               metavar='RETRY_SEC',
+                               nargs='?',
+                               type=int,
+                               default=-1,
+                               const=0,
+                               help='Check on status of server.')
 
     def run(self):
         """View Information about nblci instance.
@@ -46,7 +57,24 @@ class InfoSubCommand(BaseSubCommand):
           $ nbcli info --models device
         """
 
-        if self.args.models:
+        if self.args.status >= 0:
+            st_time = int(time())
+            d_time = 0
+            while (d_time <= self.args.status):
+                try:
+                    status = self.netbox.status()
+                    pprint(status)
+                    return
+                except:
+                    time_left = self.args.status - d_time
+                    if time_left > 0:
+                        self.logger.warning('No response from "%s".', self.netbox.base_url)
+                        self.logger.warning('Trying for %i more sec.', time_left)
+                        sleep(5)
+                d_time = int(time()) - st_time
+            self.logger.critical('No response from "%s"!', self.netbox.base_url)
+
+        elif self.args.models:
             if self.args.models == 'all':
 
                 header = ['Model', 'Lookup', 'Endpoint']
