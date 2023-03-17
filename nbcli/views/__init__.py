b2c155cd23858597747c6ba2fd8c271a6bf6a6bf
Eric Geldmacher <egeldmacher@wustl.edu>
2023-03-17 13:11:34 -0500

    Remove flake8 overrides form sorce files and add the override to pyproject.toml

--- nbcli/views/__init__.py
+++ nbcli/views/__init__.py
@@ -1,25 +1,25 @@
 """Load views used by nbprint."""
 
-from nbcli.views.tools import BaseView                          # noqa: F401
+from nbcli.views.tools import BaseView
 
-from nbcli.views.circuits import CircuitsProvidersView          # noqa: F401
-from nbcli.views.circuits import CircuitsCircuitTypesView       # noqa: F401
-from nbcli.views.circuits import CircuitsCircuitsView           # noqa: F401
+from nbcli.views.circuits import CircuitsProvidersView
+from nbcli.views.circuits import CircuitsCircuitTypesView
+from nbcli.views.circuits import CircuitsCircuitsView
 
-from nbcli.views.dcim import DcimDevicesView                  	# noqa: F401
-from nbcli.views.dcim import DcimInterfacesView           		# noqa: F401
-from nbcli.views.dcim import DcimRacksView        				# noqa: F401
-from nbcli.views.dcim import DcimRUsView        				# noqa: F401
-from nbcli.views.dcim import DcimLocationsView             		# noqa: F401
-from nbcli.views.dcim import DcimSitesView                     	# noqa: F401
+from nbcli.views.dcim import DcimDevicesView
+from nbcli.views.dcim import DcimInterfacesView
+from nbcli.views.dcim import DcimRacksView
+from nbcli.views.dcim import DcimRUsView
+from nbcli.views.dcim import DcimLocationsView
+from nbcli.views.dcim import DcimSitesView
 
-from nbcli.views.extras import ExtrasConfigContextsView  		# noqa: F401
-from nbcli.views.extras import ExtrasObjectChangesView  		# noqa: F401
+from nbcli.views.extras import ExtrasConfigContextsView
+from nbcli.views.extras import ExtrasObjectChangesView
 
-from nbcli.views.ipam import IpamAggregatesView           		# noqa: F401
-from nbcli.views.ipam import IpamIpAddressesView        		# noqa: F401
-from nbcli.views.ipam import IpamPrefixesView            		# noqa: F401
-from nbcli.views.ipam import IpamVlansView            		    # noqa: F401
+from nbcli.views.ipam import IpamAggregatesView
+from nbcli.views.ipam import IpamIpAddressesView
+from nbcli.views.ipam import IpamPrefixesView
+from nbcli.views.ipam import IpamVlansView
 
-from nbcli.views.tenancy import TenancyTenantGroupsView         # noqa: F401
-from nbcli.views.tenancy import TenancyTenantsView              # noqa: F401
+from nbcli.views.tenancy import TenancyTenantGroupsView
+from nbcli.views.tenancy import TenancyTenantsView
